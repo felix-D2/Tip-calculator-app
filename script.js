a2f5f10e13d977d6534c286fb6e309ff9030b948
Felix <davoust.felix@gmail.com>
2021-09-01 12:36:32 +0200

    end

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const totalValue = document.getElementById('totalValue');
 const tipAmountValue = document.getElementById('tipAmountValue');
 
 
-const button = document.querySelector("button");
+const button = document.querySelector(".buttonTip");
 let arrayBtn = [0, 0, 0, 0, 0];
 
-const custumTip = document.querySelector("#tip input");
+const custumTip = document.querySelector("#customTip");
+const resetBtn = document.querySelector("#reset button");
 
-console.log("custumTip", custumTip);
 
 let bill = 0;
 let people = 0;
@@ -22,6 +22,8 @@ let total = 0;
 let tip = 0;
 let tipAmount = 0;
 
+
+console.log("custumTip", custumTip);
 
 
 function updateTotal(){
@@ -40,92 +42,104 @@ function updateTotal(){
         totalValue.innerText = "$" + "0.00";
         tipAmountValue.innerText = "$" + "0.00";
     }
-    console.log("test");
 }
 
-updateTotal();
 
 
-
-
+// Input bill et number of people
 billValue.addEventListener("input", function(e){
     bill = e.target.value*1; // *1 cast pour convertir en nombre
     updateTotal();
+    resetBtn.classList.remove("disabled");
 });
-numberOfPeopleValue.addEventListener("input", function(e){
-    people = e.target.value;
-    updateTotal();
-});
-
-
-
-
-
-// Selection d'un bouton
-function btnSelecting(btnNumber, tipValue) {
-    let currentButton = button;
-    for(let i=0; i<5; i++) {
-        if (i == btnNumber && arrayBtn[i] != 1) {
-            currentButton.style.backgroundColor = "#26C2AD";
-            console.log("btn " + btnNumber + " cliqué");
-            arrayBtn[i] += 1;
-            tip = tipValue;
-        }
-        else if (i == btnNumber && arrayBtn[i] == 1) {
-            currentButton.removeAttribute("style");
-            arrayBtn[i] = 0;
-            tip = 0;
-        }
-        else if (btnNumber == -1) { // To reseting function
-            currentButton.removeAttribute("style");
-            arrayBtn[i] = 0;
-            tip = 0;
-        }
-        else {
-            currentButton.removeAttribute("style");
-            arrayBtn[i] = 0;
-        }
-        currentButton = currentButton.nextElementSibling;
-    }
-    updateTotal();
-    console.log("tip", tip);
-}
-
-custumTip.addEventListener("input", function(e){
-    tip = e.target.value;
-    updateTotal();
-});
-
 
 numberOfPeopleValue.addEventListener("input", function(e){
     people = e.target.value;
     if (people == 0) {
         erreurPeople.innerText = "Can't be zero";
         numberOfPeopleValue.classList.add("erreur");
-        //numberOfPeopleValue.style.border="solid";
-        //numberOfPeopleValue.style.border-color="red";
       } else {
         erreurPeople.innerText = "";
         numberOfPeopleValue.classList.remove("erreur");
     }
-
     updateTotal();
+    resetBtn.classList.remove("disabled");
 });
 
 
 
-
-
-let inputs = document.querySelector("input.numberOfPeople");
-
-console.log("inputs", inputs);
-
-function resetting(){
-    console.log("coucou");
-    numberOfPeopleValue.setAttribute("value", "10");
-
-    inputs.forEach(input => input.value = '');
-
-    btnSelecting(-1);
+// Selection d'un bouton tip
+function btnSelecting(btnNumber, tipValue) {
+    let currentButton = button;
+    for(let i=0; i<5; i++) {
+        if (i == btnNumber && arrayBtn[i] != 1) {
+            currentButton.style.backgroundColor = "#26C2AD";
+            custumTip.removeAttribute("style");
+            console.log("btn " + btnNumber + " cliqué");
+            arrayBtn[i] += 1;
+            tip = tipValue;
+            custumTip.value=0;
+        }
+        else if (i == btnNumber && arrayBtn[i] == 1) {
+            currentButton.removeAttribute("style");
+            custumTip.removeAttribute("style");
+            arrayBtn[i] = 0;
+            tip = 0;
+            custumTip.value=0;
+        }
+        else if (btnNumber == -1) { // To reseting function and custom tip
+            currentButton.removeAttribute("style");
+            arrayBtn[i] = 0;
+        }
+        else {  
+            currentButton.removeAttribute("style");
+            custumTip.removeAttribute("style");
+            arrayBtn[i] = 0;
+            custumTip.value=0;
+        }
+        currentButton = currentButton.nextElementSibling;
+    }
+    updateTotal();
+    resetBtn.classList.remove("disabled");
+    console.log("tip", tip);
 }
 
+
+
+//Input tip personalisé
+custumTip.addEventListener("input", function(e){
+    custumTip.style.backgroundColor = "#26C2AD";
+
+    tip = e.target.value*1;
+    updateTotal();
+    console.log("tip", tip);
+
+    btnSelecting(-1);
+    resetBtn.classList.remove("disabled");
+});
+
+
+
+// Resetting fonction
+function resetting(){
+    btnSelecting(-1);
+    bill=0;
+    tip = 0;
+    people=0;
+    updateTotal();
+
+    erreurPeople.innerText = "";
+    numberOfPeopleValue.classList.remove("erreur");
+    custumTip.removeAttribute("style");
+
+    resetBtn.classList.add("disabled");
+
+    console.log("reset");
+}
+
+
+
+
+
+resetBtn.classList.add("disabled");
+updateTotal();
